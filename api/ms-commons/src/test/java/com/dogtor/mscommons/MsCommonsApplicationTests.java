6eb5d67a75f1aec205417fbd1399eace6bd7a69c
Danilo Dassi de Araujo <danariano83@gmail.com>
2024-02-18 21:49:41 -0300

    api commons zerado

---
+++ api/ms-commons/src/test/java/com/dogtor/mscommons/MsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogtor.mscommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
