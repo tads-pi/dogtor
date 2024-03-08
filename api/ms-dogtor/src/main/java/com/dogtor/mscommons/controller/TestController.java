6b4f66d12ce986b933e20edcf095b71abe3180b7
Danilo Dassi de Araujo <danariano83@gmail.com>
2024-03-07 23:02:37 -0300

    adicionando swagger

--- api/ms-dogtor/src/main/java/com/dogtor/mscommons/controller/TestController.java
+++ api/ms-dogtor/src/main/java/com/dogtor/mscommons/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.dogtor.mscommons.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Endpoint teste", description = "Endpoint para teste de funcionamento.")
 @RequestMapping("/service")
 @RequiredArgsConstructor
 @RestController
